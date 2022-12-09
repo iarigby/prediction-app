20da6cab53b882185d7ba3e31299d0ce2fafee09
ia rigby <marvinzem@gmail.com>
2022-12-09 18:25:36 +0400

    add endpoint documentation example

--- app/api.py
+++ app/api.py
@@ -1,6 +1,13 @@
 from flask import Flask, redirect, url_for
+from flasgger import Swagger
+
 
 app = Flask(__name__)
+swagger = Swagger(app, template={
+    "info": {
+        "title": "Breast Cancer Prediction API",
+    }
+})
 
 
 @app.route("/")
@@ -10,6 +17,24 @@ def hello():
 
 @app.route("/health")
 def health_check():
+    """Health Check Endpoint
+    ---
+    definitions:
+        HealthStatus:
+            type: object
+            properties:
+                status:
+                    type: string
+                    enum: ['available']
+    responses:
+        200:
+            description:
+                application status
+            schema:
+                $ref: '#/definitions/HealthStatus'
+            examples:
+                status: 'available'
+    """
     return {
         'status': 'available'
     }
