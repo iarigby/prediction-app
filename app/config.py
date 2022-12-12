6dd41be4144ee58e0177405787b8144275d40b1e
ia rigby <marvinzem@gmail.com>
2022-12-12 13:55:36 +0400

    set swagger base path dynamically

--- app/config.py
+++ app/config.py
@@ -1,7 +1,15 @@
+import os
+
 from flasgger import Swagger
 
 swagger_config = Swagger.DEFAULT_CONFIG
 swagger_config["specs_route"] = "/docs"
+
+script_name = os.environ.get('SCRIPT_NAME')
+if script_name is None:
+    swagger_config["basePath"] = script_name
+else:
+    swagger_config["basePath"] = "/api"
 
 swagger_template = {
     "info": {
